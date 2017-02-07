daab251159152136595f2e1be0acc243eb3fb151
Elizabeth Jacobs <elizabethjjacobs@gmail.com>
2017-02-07 09:33:16 -0500

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,12 @@
 export function manageFriends(state, action){
+  switch (action.type) {
+    case 'ADD_FRIEND':
+      return {...state, friends: [...state.friends, action.payload]
+      }
+    case 'REMOVE_FRIEND':
+      var updated_friends = state.friends.filter((friend) => friend.id !== action.payload)
+      return {...state, friends: updated_friends
+      }
+    default: return state
+  }
 }
--- src/reducers/managePresents.js
+++ src/reducers/managePresents.js
@@ -1,2 +1,8 @@
 export function managePresents(state, action){
+  if (action.type === "INCREASE") {
+    return Object.assign({}, state.numberOfPresents, {numberOfPresents: state.numberOfPresents + 1})
+  } else {
+    return state
+  }
+
 }
